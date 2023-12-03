39557baca2a16321684f2e85390398f73266a203
John Ritter <john.ritter.8788@gmail.com>
2023-12-03 04:44:29 -0500

    Began styling of counter code, also started general css file

---
+++ react-projects/src/projects/counter/CounterStyled.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const CounterWrapper = styled.div`
+background-color: var(--color-2);
+max-width: 800px;
+width: 100%;
+padding: 5rem;
+text-align: center;
+border-radius: 20px;
+box-shadow: var(--shadow);
+user-select: none;
+overflow: hidden;
+
+@media(max-width: 768px){
+    
+}
+]
+`
