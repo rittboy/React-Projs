11cda40bcf860031b6d8025b67e2ff17041a03f6
John Ritter <john.ritter.8788@gmail.com>
2023-12-03 02:53:11 -0500

    Finished Accordion code

--- react-projects/src/projects/accordion/Accordion.js
+++ react-projects/src/projects/accordion/Accordion.js
@@ -3,5 +3,43 @@ import accordionData from "./accordionData";
 import { AccordionWrapper } from "./AccordionStyled";
 
 const Accordion = () =>{
-    
-}
+    const [isClicked, setIsClicked] = useState(null);
+
+    const handleIsClicked = (id) =>{
+        if(isClicked === id){
+            return setIsClicked(null);
+        }
+
+        setIsClicked(id);
+    };
+    return(
+        <>
+        <section className="section">
+            <AccordionWrapper>
+                {
+                    accordionData.map((currValue) =>{
+                        return (
+                            <div className="accordion_item" key={id}>
+                                <div className={`accordion_title ${isClicked === id ? "active" : ''}`}
+                                onClick={() => handleIsClicked(id)}
+                                >
+                                    <h2>{title}</h2>
+                                    <div className="toggle_icon">
+                                        <span></span>
+                                        <span></span>
+                                    </div>
+                                </div>
+                                <div className="accordion_content">
+                                    <p>{content}</p>
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+            </AccordionWrapper>
+        </section>
+        </>
+    );
+};
+
+export default Accordion;
